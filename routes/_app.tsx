fc2fdb2a604d22c9f816b383c88c63593559dfb8
guxgl <giovanny.lisboa@gux.cx>
2024-10-17 15:22:17 -0300

    upgrade

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,35 +1,26 @@
 import { asset, Head } from "$fresh/runtime.ts";
 import { defineApp } from "$fresh/server.ts";
-import { Context } from "deco/deco.ts";
 import Theme from "../sections/Theme/Theme.tsx";
-
+import { Context } from "@deco/deco";
 export default defineApp(async (_req, ctx) => {
-  const revision = await Context.active().release?.revision();
-
-  return (
-    <>
+    const revision = await Context.active().release?.revision();
+    return (<>
       {/* Include default fonts and css vars */}
-      <Theme colorScheme="any" />
+      <Theme colorScheme="any"/>
 
       {/* Include Icons and manifest */}
       <Head>
         {/* Enable View Transitions API */}
-        <meta name="view-transition" content="same-origin" />
+        <meta name="view-transition" content="same-origin"/>
 
         {/* Tailwind v3 CSS file */}
-        <link
-          href={asset(`/styles.css?revision=${revision}`)}
-          rel="stylesheet"
-        />
+        <link href={asset(`/styles.css?revision=${revision}`)} rel="stylesheet"/>
 
         {/* Web Manifest */}
-        <link rel="manifest" href={asset("/site.webmanifest")} />
+        <link rel="manifest" href={asset("/site.webmanifest")}/>
 
-        <style
-          dangerouslySetInnerHTML={{
-            __html: 
-            
-            `
+        <style dangerouslySetInnerHTML={{
+            __html: `
             .text-ibm {
                 font-family: "IBM Plex Mono", monospace;
             }
@@ -47,12 +38,10 @@ export default defineApp(async (_req, ctx) => {
             :root {
                 --font-family: "aqui", sans-serif !important;
             }`
-          }}
-        />
+        }}/>
       </Head>
 
       {/* Rest of Preact tree */}
       <ctx.Component />
-    </>
-  );
+    </>);
 });
