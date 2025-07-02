172cc3087fb7f1a2afd976990d00cb5b10239b48
Joao Silva <joao.silva@gaia.dev>
2025-07-02 19:05:50 +0000

    Publish 4 from staging into origin/main

--- sections/Section-2.tsx
+++ sections/Section-2.tsx
@@ -4,14 +4,12 @@ interface Props {
 }
 
 function Section2({
-  text_1 = "Made with",
-  text_2 = "Made with",
 }: Props) {
   return (
     <section class="bg-black relative z-[40]">
       <div class="container">
           <video width="100%" loop autoplay muted playsinline>
-              <source src="/gaia.mp4" type="video/mp4" />
+          <source src="/gaia_flor.mp4" type="video/mp4" />
               Seu navegador não suporta a tag de vídeo.
           </video>
       </div>
